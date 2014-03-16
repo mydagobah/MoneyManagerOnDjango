f668593bff0dc3f1341401e0473290ed81c5ee89
mydagobah <wenjunzhang.surpass@gmail.com>
2014-03-15 21:44:54 -0400

    add robot UI and year/month selection

--- MoneyManager/static/MoneyManager/myjs.js
+++ MoneyManager/static/MoneyManager/myjs.js
@@ -1,3 +1,18 @@
+window.onload = init; // set onload callback function. no ()
+
+function init() {
+    var dt = new Date();
+    var curr_year  = dt.getFullYear();
+    var curr_month = dt.getMonth() + 1;
+    var year_options = getYearList(2014, curr_year);
+    var month_options = getMonthList(curr_month);
+    var year_obj  = document.getElementById('yoptions');
+    var month_obj = document.getElementById('moptions');
+
+    fillSelect(year_obj, year_options, curr_year.toString());
+    fillSelect(month_obj, month_options, formatMonth(curr_month));
+}
+
 function drawCharts() {
     // Load the Visualization API and the piechart package.
     google.load('visualization', '1', {'packages':['corechart']});
@@ -61,4 +76,54 @@ function drawYearlyChart() {
     // Instantiate and draw our chart, passing in some options.
     var chart = new google.visualization.PieChart(document.getElementById('year-chart'));
     chart.draw(data, options);
+}
+
+// fill select options
+// input  - obj: select DOM object
+//          values: values for options
+//          selected: value as selected
+// output - none
+function fillSelect(obj, values, selected) {
+     obj.options.length = 0;
+     for (var i=0; i<values.length; i++) {
+         obj.options[i] = new Option(values[i]);
+	 if (values[i] == selected) {
+             obj.options[i].selected = true;
+	 }
+     }
+}
+
+// return a year list
+// input  - max year
+// output - a list of string from init year to max year
+function getYearList(init_year, max_year) {
+    var list = new Array();
+    for(var i=max_year; i>=init_year; i--) {
+        list.push(i.toString());
+    }
+    return list;
+}
+
+// return a month list
+// input  - max month
+// output - a list of string from 01 to max month
+function getMonthList(max_month) {
+    var init_month = 1;
+    var list = new Array();
+    for(var i=max_month; i>=init_month; i--) {
+	list.push(formatMonth(i));
+    }
+    return list;
+}
+
+// convert month number to 2 digit string
+// input:  number 1..12
+// output: string 01..12
+function formatMonth(month) {
+    var prefix = "0";
+    if (month < 10) {
+        return prefix.concat(month.toString());
+    } else {
+	return month.toString();
+    }
 }
